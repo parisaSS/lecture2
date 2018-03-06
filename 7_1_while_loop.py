46fdb8da5b67eb3515f911c715567f054ecfb750
parisaSS <36893508+parisaSS@users.noreply.github.com>
2018-03-06 06:56:31 -0800

    Update 7_1_while_loop.py
    شرط حلقه برای احراز هویت از علی به پریسا تغییر پیدا کرد

--- 7_1_while_loop.py
+++ 7_1_while_loop.py
@@ -2,7 +2,7 @@
 ## example of a simple while loop#
 ##################################
 a=input("please enter your name\n")
-while a != "Ali":
+while a != "parisa":
     print("oops you are not a legal user")
     a = input("please enter your name\n")
 print("thanks for your registration")
