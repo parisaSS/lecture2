67a79c0afcc88b241ffaa14c4bd8f741cb37377a
parisaSS <36893508+parisaSS@users.noreply.github.com>
2018-03-06 06:50:49 -0800

    Update 7_2_while_vs_for_loop.py
      استپ از 2 به 4 تغییر یافت یعنی سال 4 تا 4 تا جلو رود تا اینکه شرط نقض شود
    وشرط حلقه به سال 2030تغییر یافت

--- 7_2_while_vs_for_loop.py
+++ 7_2_while_vs_for_loop.py
@@ -1,11 +1,11 @@
 year=int(input("please enter year\n"))
 temp_year=year
-while year<2029:
+while year<2030:
     year+=1
     print(year)
 
 print("inside for loop")
 
 year=int(input("please enter year\n"))
-for x in range(temp_year,2029,2):
+for x in range(temp_year,2030,4):
     print(x)
