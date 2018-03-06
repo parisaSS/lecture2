140aeeee2b4e0222b5d56b73f9d4802834f0068e
parisaSS <36893508+parisaSS@users.noreply.github.com>
2018-03-06 07:06:30 -0800

    Update 6_1_if_statement.py
    shart az (if)(elif)(else) B (if)(else) tagheer peida kard
    adaad niz tagheer kark

--- 6_1_if_statement.py
+++ 6_1_if_statement.py
@@ -1,13 +1,9 @@
 ###################################################
 ## these examples shows different type of if-stmt #
 ###################################################
-a=5
-b=6
+a=9
+b=7
 if(a == b):
     print("a == b")
-if(a > b):
-    print("a > b")
-elif(a<b):
-    print("a < b")
 else:
     pass # do nothing
